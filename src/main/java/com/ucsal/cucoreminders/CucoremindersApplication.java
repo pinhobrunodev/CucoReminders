b12a50b44535444006ea808923b982c6f78c778e
pinhobrunodev <iaishowcs@gmail.com>
2021-12-08 18:46:15 -0300

    Projeto iniciado

---
+++ src/main/java/com/ucsal/cucoreminders/CucoremindersApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.cucoreminders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucoremindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucoremindersApplication.class, args);
+	}
+
+}
