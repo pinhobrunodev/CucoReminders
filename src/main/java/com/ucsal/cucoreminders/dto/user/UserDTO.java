05bc3af69c28aca96315dfd1aad9f87f085036d5
pinhobrunodev <iaishowcs@gmail.com>
2022-11-03 05:42:21 -0300

    fix valid pass

--- src/main/java/com/ucsal/cucoreminders/dto/user/UserDTO.java
+++ src/main/java/com/ucsal/cucoreminders/dto/user/UserDTO.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserDTO {
 
     private String fullName;
+    @NotBlank(message = "Campo requerido.")
+    @Size(min = 5,max = 20,message = "A senha precisa estar entre 5 e 20 caracteres")
     private String password;
 }
